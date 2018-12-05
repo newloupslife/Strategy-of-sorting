a8ee47aa3501374dbb0fbe0d1acb60e92bcae87f
newloupslife <uchkin14@yandex.ru>
2018-12-05 21:14:41 +0200

    Realized diff sort ways

--- src/StategySorting.php
+++ src/StategySorting.php
@@ -1,11 +1,4 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: newloupslife
- * Date: 12/5/18
- * Time: 7:55 PM
- */
-
 namespace App;
 
 
@@ -17,9 +10,43 @@ class StategySorting
     public function __construct($data)
     {
         if (!\is_array($data)){
-            throw new \LogicException(" %s not an array. Please enter an array of numbers or strings ");
+            throw new \LogicException("Input data not an array. Please enter an array of numbers or strings ");
         }
 
         $this->sortingDATA = $data;
     }
+
+    public function sortDATA()
+    {
+        switch ($this->sortingSTRATEGY)
+            {
+            case "up":
+                asort($this->sortingDATA);
+            break;
+
+            case "down":
+                arsort($this->sortingDATA);
+            break;
+            }
+
+    }
+
+    public function sortStrategy($strategy)
+    {
+        if ($strategy != "up" && $strategy != "down")
+        {
+            throw new \LogicException("Wrong strategy, please choose between 'up' and 'down' sort strategy");
+        }
+
+        if ($strategy == "down")
+        {
+            return $this->sortingSTRATEGY = $strategy;
+        }
+    }
+
+    public function getData()
+    {
+        return $this->sortingDATA;
+    }
+
 }
